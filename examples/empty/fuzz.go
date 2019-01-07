f41bea58890082475fd23041a6bfb92c22e53953
thepudds <20628140+thepudds@users.noreply.github.com>
2019-01-07 03:11:49 -0500

    examples: add two empty fuzzing functions

---
+++ examples/empty/fuzz.go
@@ -0,0 +1,11 @@
+package fuzzempty
+
+// FuzzEmpty is an empty placeholder fuzzing function
+func FuzzEmpty(data []byte) int {
+	return 0
+}
+
+// FuzzAnotherEmpty is another empty placeholder fuzzing function
+func FuzzAnotherEmpty(data []byte) int {
+	return 0
+}
