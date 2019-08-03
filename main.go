a212f4448146299d09f30b594bf70bb1730ef986
thepudds <20628140+thepudds@users.noreply.github.com>
2019-08-03 15:40:57 -0400

    main.go: multiple corpus locations as input to fuzzing, change default dest

--- main.go
+++ main.go
@@ -6,9 +6,10 @@
 // 1. cacheDir is the location for the instrumented binary, and would typically be something like:
 //      GOPATH/pkg/fuzz/linux_amd64/619f7d77e9cd5d7433f8/fmt.FuzzFmt
 // 2. fuzzDir is the directory supplied via the -fuzzdir argument, and contains the workDir.
-// 3. workDir is passed to go-fuzz as the -workdir argument.
-//      if -fuzzdir is specified,  workDir is fuzzdir/<fuzzname>
-//      if -fuzzdir is not specified, workDir is pkgpath/testdata/fuzz/<fuzzname>
+// 3. workDir is passed to go-fuzz-build and go-fuzz as the -workdir argument.
+//      if -fuzzdir is not specified:  workDir is GOPATH/pkg/fuzz/corpus/<import-path>/<func>
+//      if -fuzzdir is '/some/path':   workDir is /some/path/<import-path>/<func>
+//      if -fuzzdir is 'testdata':     workDir is <pkg-dir>/testdata/fuzz/<func>
 package main
 
 import (
@@ -79,7 +80,7 @@ func fzgoMain() int {
 
 	if os.Args[1] != "test" {
 		// pass through to 'go' command
-		err := fuzz.ExecGo(os.Args[1:], nil)
+		err = fuzz.ExecGo(os.Args[1:], nil)
 		if err != nil {
 			// ExecGo prints error if 'go' tool is not in path.
 			// Other than that, we currently rely on the 'go' tool to print any errors itself.
@@ -147,7 +148,7 @@ func fzgoMain() int {
 		fmt.Printf("fzgo: found functions %s\n", strings.Join(names, ", "))
 	}
 
-	// build our instrumented code, or find it already built in the fzgo cache
+	// build our instrumented code, or find if is is already built in the fzgo cache
 	var targets []fuzz.Target
 	for _, function := range functions {
 		target, err := fuzz.Instrument(function, flagVerbose)
@@ -168,13 +169,14 @@ func fzgoMain() int {
 	timeQuantum := 5 * time.Second
 	for {
 		for _, target := range targets {
-			// pull our last bit of info out of our arguments, then start fuzzing.
-			// TODO: Currently calculating this > 1 time in main.go.
-			var workDir string
-			if flagFuzzDir == "" {
-				workDir = filepath.Join(target.UserFunc.PkgDir, "testdata", "fuzz", target.FuzzName())
-			} else {
-				workDir = filepath.Join(flagFuzzDir, target.FuzzName())
+			// pull our last bit of info out of our arguments.
+			workDir := determineWorkDir(target.UserFunc, flagFuzzDir)
+
+			// seed our workDir with any other corpus that might exist from other known locations.
+			// see comment for copyCachedCorpus for discussion of current behavior vs. desired behavior.
+			if err = copyCachedCorpus(target.UserFunc, workDir); err != nil {
+				fmt.Println("fzgo:", err)
+				return OtherErr
 			}
 
 			// determine how long we will execute this particular fuzz invocation.
@@ -216,7 +218,7 @@ func fzgoMain() int {
 // args is os.Args
 func verifyCorpus(args []string) int {
 	// we do this by first searching for any fuzz func ("." regexp) in our package pattern.
-	// TODO: move this elsewhere. Taken from fuzz.ParseArgs.
+	// TODO: move this elsewhere? Taken from fuzz.ParseArgs, but we can't use fuzz.ParseArgs as is.
 	testPkgPatterns, nonPkgArgs, err := fuzz.FindPkgs(args[2:])
 	if err != nil {
 		fmt.Println("fzgo:", err)
@@ -239,27 +241,112 @@ func verifyCorpus(args []string) int {
 	}
 	// TODO: should we get -v? E.g., something like:
 	// _, verbose := fuzz.FindTestFlag(os.Args[2:], []string{"v"})
+
 	status := Success
 	for _, function := range functions {
-		fuzzName := function.FuzzName()
-		var workDir string
+
+		// work through how many places we need to check based on
+		// what the user specified in flagFuzzDir.
+		var dirsToCheck []string
+
+		// we always check the "testdata" dir if it exists.
+		testdataWorkDir := determineWorkDir(function, "testdata")
+		dirsToCheck = append(dirsToCheck, testdataWorkDir)
+
+		// we also always check under  GOPATH/pkg/fuzz/corpus/... if it exists.
+		gopathPkgWorkDir := determineWorkDir(function, "")
+		dirsToCheck = append(dirsToCheck, gopathPkgWorkDir)
+
+		// see if we need to check elsewhere as well.
 		if flagFuzzDir == "" {
-			workDir = filepath.Join(function.PkgDir, "testdata", "fuzz", fuzzName)
+			// nothing else to do; the user did not specify a dir.
+		} else if flagFuzzDir == "testdata" {
+			// nothing else to do; we already added testdata dir.
 		} else {
-			workDir = filepath.Join(flagFuzzDir, fuzzName)
+			// the user supplied a destination
+			userWorkDir := determineWorkDir(function, flagFuzzDir)
+			dirsToCheck = append(dirsToCheck, userWorkDir)
 		}
-		err := fuzz.VerifyCorpus(function, workDir, nonPkgArgs)
-		if err == fuzz.ErrGoTestFailed {
-			// 'go test' itself should have printed an informative error,
-			// so here we just set a non-zero status code and continue.
-			status = OtherErr
-		} else if err != nil {
-			fmt.Println("fzgo:", err)
-			return OtherErr
+
+		// we have 2 or 3 places to check
+		for _, workDir := range dirsToCheck {
+			if !fuzz.PathExists(workDir) {
+				// this workDir does not exist, so skip
+				continue
+			}
+
+			err := fuzz.VerifyCorpus(function, workDir, nonPkgArgs)
+			if err == fuzz.ErrGoTestFailed {
+				// 'go test' itself should have printed an informative error,
+				// so here we just set a non-zero status code and continue.
+				status = OtherErr
+			} else if err != nil {
+				fmt.Println("fzgo:", err)
+				return OtherErr
+			}
 		}
 	}
 
 	return status
+}
+
+// determineWorkDir translates from the user's specified -fuzzdir to an actual
+// location on disk, including the default location if the user does not specify a -fuzzdir.
+func determineWorkDir(function fuzz.Func, requestedFuzzDir string) string {
+	var workDir string
+	importPathDirs := filepath.FromSlash(function.PkgPath) // convert import path into filepath
+	if requestedFuzzDir == "" {
+		// default to GOPATH/pkg/fuzz/corpus/import/path/<func>
+		gp := fuzz.Gopath()
+		workDir = filepath.Join(gp, "pkg", "fuzz", "corpus", importPathDirs, function.FuncName)
+	} else if requestedFuzzDir == "testdata" {
+		// place under the package of interest in the testdata directory.
+		workDir = filepath.Join(function.PkgDir, "testdata", "fuzz", function.FuncName)
+	} else {
+		// requestedFuzzDir was specified to be an actual directory.
+		// still use the import path to handle fuzzing multiple functions across multiple packages.
+		workDir = filepath.Join(requestedFuzzDir, importPathDirs, function.FuncName)
+	}
+	return workDir
+}
+
+// copyCachedCorpus desired bheavior (or at least proposed-by-me behavior):
+//     1. if destination corpus location doesn't exist, seed it from GOPATH/pkg/fuzz/corpus/import/path/<fuzzfunc>
+//     2. related: fuzz while reading from all known locations that exist (e.g,. testdata if it exists, GOPATH/pkg/fuzz/corpus/...)
+//
+// However, 2. is not possible currently to do directly with dvyukov/go-fuzz for more than 1 corpus.
+//
+// Therefore, the current behavior of copyCachedCorpus approximates 1. and 2. like so:
+//     1'. always copy all known corpus entries to the destination corpus location in all cases.
+//
+// Also, that current behavior could be reasonable for the proposed behavior in the sense that it is simple.
+// Filenames that already exist in the destination are not updated.
+// TODO: it is debatable if it should copy crashers and suppressions as well.
+// For clarity, it only copies the corpus directory itself, and not crashers and supressions.
+// This avoids making sometone think they have a new crasher after copying a crasher to a new location, for example,
+// especially at this current prototype phase where the crasher reporting in
+// go-fuzz does not know anything about multi-corpus locations.
+func copyCachedCorpus(function fuzz.Func, dstWorkDir string) error {
+	dstCorpusDir := filepath.Join(dstWorkDir, "corpus")
+
+	gopathPkgWorkDir := determineWorkDir(function, "")
+	testdataWorkDir := determineWorkDir(function, "testdata")
+
+	for _, srcWorkDir := range []string{gopathPkgWorkDir, testdataWorkDir} {
+		srcCorpusDir := filepath.Join(srcWorkDir, "corpus")
+		if srcCorpusDir == dstCorpusDir {
+			// nothing to do
+			continue
+		}
+		if fuzz.PathExists(srcCorpusDir) {
+			// copyDir will create dstDir if needed, and won't overwrite files
+			// in dstDir that already exist.
+			if err := fuzz.CopyDir(dstCorpusDir, srcCorpusDir); err != nil {
+				return fmt.Errorf("failed seeding destination corpus: %v", err)
+			}
+		}
+	}
+	return nil
 }
 
 func usage(fs *flag.FlagSet) func() {
