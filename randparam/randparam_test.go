23614ce61f6202a1d7f42b8b0fc154fa3cfcdb09
thepudds <20628140+thepudds@users.noreply.github.com>
2019-08-10 15:55:54 -0400

    randparam_test.go: more sophisticated string encoding for rich parameters

--- randparam/randparam_test.go
+++ randparam/randparam_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestFuzzerStringsBytes_Fuzz(t *testing.T) {
+func TestFuzzingParams(t *testing.T) {
 
 	t.Run("string - 8 byte length, 8 bytes of string input", func(t *testing.T) {
 		input := append([]byte{0x8}, []byte("12345678")...)
@@ -47,13 +47,74 @@ func TestFuzzerStringsBytes_Fuzz(t *testing.T) {
 
 	t.Run("string - 9 byte length, 2 bytes of string input", func(t *testing.T) {
 		input := append([]byte{0x9}, []byte("12")...)
-		want := string(append([]byte("12"), []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}...))
+		want := ""
 
 		fuzzer := NewFuzzer(input)
 		var got string
 		fuzzer.Fuzz(&got)
 		if diff := cmp.Diff(want, got); diff != "" {
 			t.Errorf("fuzzer.Fuzz() mismatch (-want +got):\n%s", diff)
+		}
+	})
+
+	t.Run("string - zero length string explicitly encoded", func(t *testing.T) {
+		longByteSlice := make([]byte, 1000)
+		input := append([]byte{0xFF}, longByteSlice...)
+		want := ""
+
+		fuzzer := NewFuzzer(input)
+		var got string
+		fuzzer.Fuzz(&got)
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("fuzzer.Fuzz() mismatch (-want +got):\n%s", diff)
+		}
+	})
+
+	t.Run("string - skip 0x0 size fields", func(t *testing.T) {
+		input := append([]byte{0x0, 0x0, 0x2}, []byte("12")...)
+		want := "12"
+
+		fuzzer := NewFuzzer(input)
+		var got string
+		fuzzer.Fuzz(&got)
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("fuzzer.Fuzz() mismatch (-want +got):\n%s", diff)
+		}
+	})
+
+	t.Run("string - two strings", func(t *testing.T) {
+		input := append([]byte{0x1, 0x42, 0x2, 0x43, 0x44})
+		want1 := string([]byte{0x42})
+		want2 := string([]byte{0x43, 0x44})
+
+		fuzzer := NewFuzzer(input)
+		var got1, got2 string
+		fuzzer.Fuzz(&got1)
+		fuzzer.Fuzz(&got2)
+
+		if diff := cmp.Diff(want1, got1); diff != "" {
+			t.Errorf("fuzzer.Fuzz() mismatch (-want1 +got1):\n%s", diff)
+		}
+		if diff := cmp.Diff(want2, got2); diff != "" {
+			t.Errorf("fuzzer.Fuzz() mismatch (-want2 +got2):\n%s", diff)
+		}
+	})
+
+	t.Run("string - exactly run out of bytes", func(t *testing.T) {
+		input := append([]byte{0x1, 0x42})
+		want1 := string([]byte{0x42})
+		want2 := ""
+
+		fuzzer := NewFuzzer(input)
+		var got1, got2 string
+		fuzzer.Fuzz(&got1)
+		fuzzer.Fuzz(&got2)
+
+		if diff := cmp.Diff(want1, got1); diff != "" {
+			t.Errorf("fuzzer.Fuzz() mismatch (-want1 +got1):\n%s", diff)
+		}
+		if diff := cmp.Diff(want2, got2); diff != "" {
+			t.Errorf("fuzzer.Fuzz() mismatch (-want2 +got2):\n%s", diff)
 		}
 	})
 
@@ -107,7 +168,7 @@ func TestFuzzerStringsBytes_Fuzz(t *testing.T) {
 		}
 	})
 
-	t.Run("int32 - 4 bytes input", func(t *testing.T) {
+	t.Run("int32 - 4 bytes input with zeros", func(t *testing.T) {
 		input := []byte{0x42, 0x00, 0x00, 0x00}
 		want := int32(0x42)
 
@@ -119,4 +180,15 @@ func TestFuzzerStringsBytes_Fuzz(t *testing.T) {
 		}
 	})
 
+	t.Run("int32 - 1 byte input", func(t *testing.T) {
+		input := []byte{0x42}
+		want := int32(0x42)
+
+		fuzzer := NewFuzzer(input)
+		var got int32
+		fuzzer.Fuzz(&got)
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("fuzzer.Fuzz() mismatch (-want +got):\n%s", diff)
+		}
+	})
 }
